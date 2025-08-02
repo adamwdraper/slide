0b509190e6f87499d0d36bfd4031050db11df102
Adam Draper <adamwdraper@gmail.com>
2025-08-01 21:26:04 -0700

    Bump space-monkey version to 0.3.0

--- packages/space-monkey/space_monkey/__init__.py
+++ packages/space-monkey/space_monkey/__init__.py
@@ -14,7 +14,7 @@ from .message_classifier_prompt import format_classifier_prompt
 from .utils import get_logger
 
 # Version
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 # Main exports
 __all__ = [
