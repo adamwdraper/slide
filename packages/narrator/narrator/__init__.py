25f2f2a84787a81414fd25a511414600edfb5cd4
Adam Draper <adamwdraper@gmail.com>
2025-08-01 21:25:58 -0700

    Bump narrator version to 0.3.0

--- packages/narrator/narrator/__init__.py
+++ packages/narrator/narrator/__init__.py
@@ -8,7 +8,7 @@ from .models.thread import Thread
 from .models.message import Message
 from .models.attachment import Attachment
 
-__version__ = "0.2.2"
+__version__ = "0.3.0"
 __all__ = [
     "ThreadStore",
     "FileStore", 
