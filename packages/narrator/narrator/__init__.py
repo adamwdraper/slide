240afb6efaeaf9926627044569b14050d3f4f2a2
Adam Draper <adamwdraper@gmail.com>
2025-08-08 12:30:49 -0600

    Bump narrator version to 0.4.0 and update dependent package constraints

--- packages/narrator/narrator/__init__.py
+++ packages/narrator/narrator/__init__.py
@@ -8,7 +8,7 @@ from .models.thread import Thread
 from .models.message import Message
 from .models.attachment import Attachment
 
-__version__ = "0.3.0"
+__version__ = "0.4.0"
 __all__ = [
     "ThreadStore",
     "FileStore", 
